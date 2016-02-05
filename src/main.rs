1bffb62e2a2dc5741c2f0efb1ca499132f20930b
Aleksey Vorona <avorona@xmatters.com>
2016-02-05 12:27:40 -0800

    Compute method complexity

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,9 @@ fn main() {
     println!("code size:    {}", get_total_code_size(&class));
     for method in &class.methods {
         println!("\tmethod: {}{}", get_string(&class, method.name_index as usize), get_string(&class, method.descriptor_index as usize));
-        println!("\t\tsize:  {}", get_method_size(&method));
-        println!("\t\tlines: {}", get_method_lines(&method));
+        println!("\t\tsize:       {}", get_method_size(&method));
+        println!("\t\tlines:      {}", get_method_lines(&method));
+        println!("\t\tcomplexity: {}", get_method_complexity(&method));
         // println!("{:?}", method);
     }
 
@@ -78,4 +79,56 @@ fn get_opt_line_number(line: Option<&LineNumber>) -> u16 {
     line.map(|x| x.line_number).unwrap_or(0)
 }
 
+fn get_method_complexity(method: &Method) -> u16 {
+    method.attributes.iter().fold(0, |sum, a| sum + match a {
+            &Attribute::Code{ref code, ..} => get_code_complexity(code),
+            _ => 0
+        })
+}
+
+/// Using complexity definition from http://www.leepoint.net/principles_and_practices/complexity/complexity-java-method.html
+/// Start with 1 for the method. Add one for each of the following flow-related elements that are found in the method.
+/// Returns     Each return that isn't the last statement of a method.
+/// Selection   if, else, case, default.
+/// Loops   for, while, do-while, break, and continue.
+/// Operators   &&, ||, ?, and :
+/// Exceptions  catch, finally, throw, or throws clause.
+/// Bytecode reference: http://www.angelibrary.com/computer/java_21days/ch32.htm
+fn get_code_complexity(code: &Vec<(u32, Instruction)>) -> u16 {
+    // println!("{:?}", code);
+    // We start from zero to account for always-present final return - it will add the starting one for us.
+    code.iter().fold(0, |sum, instruction| sum + match instruction.1 {
+        Instruction::areturn => 1,
+        Instruction::athrow => 1,
+        Instruction::dreturn => 1,
+        Instruction::freturn => 1,
+        Instruction::if_acmpeq(..) => 1,
+        Instruction::if_acmpne(..) => 1,
+        Instruction::if_icmpeq(..) => 1,
+        Instruction::if_icmpne(..) => 1,
+        Instruction::if_icmplt(..) => 1,
+        Instruction::if_icmpge(..) => 1,
+        Instruction::if_icmpgt(..) => 1,
+        Instruction::if_icmple(..) => 1,
+        Instruction::ifeq(..) => 1,
+        Instruction::ifne(..) => 1,
+        Instruction::iflt(..) => 1,
+        Instruction::ifge(..) => 1,
+        Instruction::ifgt(..) => 1,
+        Instruction::ifle(..) => 1,
+        Instruction::ifnonnull(..) => 1,
+        Instruction::ifnull(..) => 1,
+        Instruction::ireturn => 1,
+        Instruction::lookupswitch(_, ref jumps) => jumps.len() as u16, // tricky - lets use the size of the jump table
+        Instruction::lreturn => 1,
+        Instruction::return_ => 1,
+        Instruction::tableswitch(_, _, ref jumps) => jumps.len() as u16, // tricky - lets use the size of the jump table
+        // Subroutines in the method. Currently - do not count them towards the complexity. They are used to compile "finally", but I do not see them as branching really. Finally block is executed every time
+        // Instruction::jsr(..) => 1,
+        // Instruction::jsr_w(..) => 1,
+        // Instruction::ret => 1,
+        // Instruction::ret_w => 1,
+        _ => 0
+    })
+}
 

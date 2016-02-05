0a4aa3268d2440d66b7628cf5f15f5d81ab2a765
Aleksey Vorona <avorona@xmatters.com>
2016-02-05 11:35:57 -0800

    Added lines of code computation

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,25 @@ fn main() {
     println!("method count: {}", class.methods.len());
     println!("field count:  {}", class.fields.len());
     println!("code size:    {}", get_total_code_size(&class));
+    for method in &class.methods {
+        println!("\tmethod: {}{}", get_string(&class, method.name_index as usize), get_string(&class, method.descriptor_index as usize));
+        println!("\t\tsize:  {}", get_method_size(&method));
+        println!("\t\tlines: {}", get_method_lines(&method));
+        // println!("{:?}", method);
+    }
 
 
-    // println!("{:?}", class);
 
     println!("======================================================");
     println!("Done!");
 }
 
+/// Get constant from a pool, correcting for java's 1-based indexes.
 fn get_const(class: &Class, i: usize) -> &ConstantPoolInfo {
     &class.constant_pool[i - 1]
 }
 
+/// Get string from constant pool
 fn get_string(class: &Class, index: usize) -> String {
     match get_const(class, index) {
         &ConstantPoolInfo::Utf8(ref s) => s.clone(),
@@ -45,6 +52,8 @@ fn get_total_code_size(class: &Class) -> usize {
     class.methods.iter().fold(0usize, |sum, method| sum + get_method_size(method))
 }
 
+/// Compute method size in byte code instructions
+/// If multiple code segments are present in a method (not a javac output) sum it up
 fn get_method_size(method: &Method) -> usize {
     method.attributes.iter().fold(0usize, |sum, a| sum + match a {
             &Attribute::Code{ref code, ..} => code.len(),
@@ -52,6 +61,21 @@ fn get_method_size(method: &Method) -> usize {
         })
 }
 
+/// Compute method size in lines of code.
+/// If multiple code segments or multiple line number attrbitues are present in a method (not a javac output) sum it up
+fn get_method_lines(method: &Method) -> u16 {
+    method.attributes.iter().fold(0, |sum, a| sum + match a {
+            &Attribute::Code{ref attributes, ..} => attributes.iter().fold(0, |sum, a| sum + match a {
+                &Attribute::LineNumberTable(ref line_table) => get_opt_line_number(line_table.last()) - get_opt_line_number(line_table.first()),
+                _ => 0
+            }),
+            _ => 0
+        })
 
+}
+
+fn get_opt_line_number(line: Option<&LineNumber>) -> u16 {
+    line.map(|x| x.line_number).unwrap_or(0)
+}
 
 
